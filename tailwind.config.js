3785244387930476e001bb65dfec3c827a1a40f3
FabianRaja <fabiraja21052002@gmail.com>
2025-04-29 12:23:39 +0530

    require error solved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
 export default {
   content: [
     "./index.html",
@@ -11,5 +12,5 @@ export default {
       'lg': '456px'
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 }
